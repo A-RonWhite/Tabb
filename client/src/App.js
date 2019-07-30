573273b8649ab1095d4a3c839a8f0e0fc4070c9d
Aaron White <aaronwhite1234@hotmail.co.uk>
2019-07-30 10:51:45 +0100

    link context and global state setup

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,22 @@ import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import './App.css';
+import LinkState from './context/link/LinkState';
 
 const App = () => (
-  <Router>
-    <Fragment className="App">
-      <Navbar />
-      <div className="container">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-        </Switch>
-      </div>
-    </Fragment>
-  </Router>
+  <LinkState>
+    <Router>
+      <Fragment className="App">
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
+  </LinkState>
 );
 
 export default App;
---
+++ client/src/context/link/LinkState.js
@@ -0,0 +1,66 @@
+import React, { useReducer } from 'react';
+import uuid from 'uuid';
+import LinkContext from './linkContext';
+import linkReducer from './linkReducer';
+import {
+  ADD_LINK,
+  DELETE_LINK,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_LINK,
+  FILTER_LINKS,
+  CLEAR_FILTER,
+} from '../types';
+
+const LinkState = props => {
+  const initialState = {
+    links: [
+      {
+        id: 1,
+        name: 'league',
+        hyperLink: 'wwww.league.com',
+        tag: 'league ting',
+      },
+      {
+        id: 2,
+        name: 'google',
+        hyperLink: 'wwww.google.com',
+        tag: 'google ting',
+      },
+      {
+        id: 3,
+        name: 'udemy',
+        hyperLink: 'wwww.udemy.com',
+        tag: 'udemy ting',
+      },
+    ],
+  };
+
+  const [state, dispatch] = useReducer(linkReducer, initialState);
+
+  // Add Link
+
+  // Delete Link
+
+  // Set current Link
+
+  // Clear current contact
+
+  // Update link
+
+  // Filter link
+
+  // Clear link
+
+  return (
+    <LinkContext.Provider
+      value={{
+        links: state.links,
+      }}
+    >
+      {props.children}
+    </LinkContext.Provider>
+  );
+};
+
+export default LinkState;
