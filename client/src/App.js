8cb48d6bd57c78121222191e1e57323982680c50
Aaron White <aaronwhite1234@hotmail.co.uk>
2019-09-10 13:01:07 +0100

    Alert context and func added

--- client/src/App.js
+++ client/src/App.js
@@ -5,26 +5,31 @@ import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alerts from './components/layout/Alerts';
 import './App.css';
 
 // Global State
 import LinkState from './context/link/LinkState';
 import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
 
 const App = () => (
   <AuthState>
     <LinkState>
-      <Router>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/about" component={About} />
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-          </Switch>
-        </div>
-      </Router>
+      <AlertState>
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Alerts />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </Switch>
+          </div>
+        </Router>
+      </AlertState>
     </LinkState>
   </AuthState>
 );
